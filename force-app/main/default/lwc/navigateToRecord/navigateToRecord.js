43b7a109cb000398cc8b5cb89f0849c78cf266a3
Poonam Rathore <poonam.rathore@mtxb2b.com>
2023-12-12 10:02:36 +0530

    last min update

--- force-app/main/default/lwc/navigateToRecord/navigateToRecord.js
+++ force-app/main/default/lwc/navigateToRecord/navigateToRecord.js
@@ -1,6 +1,5 @@
 import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
-
 export default class NavigateToRecord extends NavigationMixin(
     LightningElement
 ) {
