24d33b3295fd7ba813cd49dc0fe150f1be2cfb9b
Poonam Rathore <poonam.rathore@mtxb2b.com>
2023-12-14 10:14:55 +0530

    Task 1 updates

--- force-app/main/default/lwc/navigateToRecord/navigateToRecord.js
+++ force-app/main/default/lwc/navigateToRecord/navigateToRecord.js
@@ -1,10 +1,12 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api,track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
 export default class NavigateToRecord extends NavigationMixin(
     LightningElement
 ) {
     @api recordId;
+    @track param1 = 'One';
+    @track param2 = 'Two';
     connectedCallback() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
